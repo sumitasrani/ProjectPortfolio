47ccba2cba1608bc47a8c0bc3ae0bc8d33b7cbe2
sumitasrani <hello@sumitasrani.com>
2023-08-02 00:46:55 +0530

    Add columns, image, description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/Sumit Asrani.png")
+
+with col2:
+    st.title("Sumit Asrani")
+    content = """
+                Hi, I am Sumit Asrani! I am a Python programmer.\n
+                I am transitioning from Content (Marketing) to Coding.\n
+                This project is to showcase various python projects
+                I have built on my journey to learn Python.
+            """
+    st.info(content)
